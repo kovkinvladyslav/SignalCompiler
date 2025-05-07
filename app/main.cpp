739b16407f333f27a383e3938338acc1140156e2
kovkinvlad <kovkinvlad@gmail.com>
2025-05-07 17:04:31 +0300

    update main logic for testing

--- app/main.cpp
+++ app/main.cpp
@@ -8,20 +8,20 @@
 #include <Lexer/LexemString.h>
 #include <Parser/parser.h>
 #include <Lexer/Error.h>
-
+#include <Generator/generator.h>
 int main(int argc, char** argv){
 
-    if(argc != 2){
-        std::cerr << "Compilation error: Path of file is required!";
-        return 1;
-    }
-    std::string testPath = "./source/tests/";
-    std::string testFile = argv[1];
+    // if(argc != 2){
+    //     std::cerr << "Compilation error: Path of file is required!";
+    //     return 1;
+    // }
+    // std::string testPath = "./source/tests/";
+    // std::string testFile = argv[1];
 
-    if (testFile.find('/') == std::string::npos) {
-        testFile = testPath + testFile;
-    }
-
+    // if (testFile.find('/') == std::string::npos) {
+    //     testFile = testPath + testFile;
+    // }
+    std::string testFile = "../../tests/trueTest1.txt";
 
     Tables LexTable;
     Attributes attributes;
@@ -31,9 +31,12 @@ int main(int argc, char** argv){
     scan_result.Output();
     LexTable.OutputAllTables();
     Parser parser(scan_result, LexTable);
-    parser.generate_tree()->output_tree();
+    auto generated_Tree = parser.generate_tree();
+    generated_Tree->output_tree();
+    Generator gen(scan_result);
+    gen.generate_code(generated_Tree);
+    gen.output_listing();
     errorLogger.printErrors();
-
-
     
+
 }
